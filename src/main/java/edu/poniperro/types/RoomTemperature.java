c69f60be5b9d66e4f272ef17f3f4cb46fe27ce7e
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-04-03 12:57:56 +0200

    add adt to control temperature

--- src/main/java/edu/poniperro/types/RoomTemperature.java
+++ src/main/java/edu/poniperro/types/RoomTemperature.java
@@ -1,2 +1,21 @@
-package edu.poniperro.types;public class RoomTemperature {
+package edu.poniperro.types;
+
+public class RoomTemperature {
+   private double temperature;
+
+   public RoomTemperature(double temperature) {
+       this.temperature = temperature;
+   }
+
+   public double getTemperature() {
+       return this.temperature;
+   }
+
+   public void setTemperature(double temperature) {
+       this.temperature = temperature;
+   }
+
+   public void incrementTemperature(double increment) {
+       this.temperature += increment;
+   }
 }
