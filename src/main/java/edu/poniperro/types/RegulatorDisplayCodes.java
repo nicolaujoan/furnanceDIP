2964e0e4441f6b7108114f9388d82e1943df662d
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-04-03 12:54:44 +0200

    add enum type to display regulator codes

--- src/main/java/edu/poniperro/types/RegulatorDisplayCodes.java
+++ src/main/java/edu/poniperro/types/RegulatorDisplayCodes.java
@@ -1,2 +1,15 @@
-package edu.poniperro.types;public enum RegulatorDisplayCodes {
+package edu.poniperro.types;
+
+public enum RegulatorDisplayCodes {
+    HEATING("waiting..."), WAITING("heating..."), ON("turning on..."),
+    OFF("turning off...");
+
+    private String message;
+
+    RegulatorDisplayCodes(String message) {
+        this.message  = message;
+    }
+    private String getMessage() {
+        return this.message;
+    }
 }
